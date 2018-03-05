9411ff756538a6e341e9d84ea1717e1f8eedb5f0
miaortizma <miaortizma@unal.edu.co>
2018-03-04 23:30:26 -0500

    Insert fix

--- insertion.py
+++ insertion.py
@@ -10,6 +10,11 @@ import itertools as it
 import matplotlib.pyplot as plt
 
 
+
+'''
+Returns the array after insertion
+and the number of comparations, swaps and other instructions made
+'''
 def insert(arr, j):
     i = j
     while(i > 0 and arr[i] < arr[i-1]):
@@ -18,7 +23,9 @@ def insert(arr, j):
     if(i == 0):
         comparations = 2*(j) + 1
         others = 1 + (j - 1)
-    else:
+    elif(i == j):
+        comparations = 2
+    else:    
         comparations = 2*(j - i)
     swaps = j - i
     others = 1 + (j - i)
@@ -59,14 +66,54 @@ def graph(comparations, swaps, others):
     plt.hist(allOps, bins='auto')
     plt.show()
 
+'''
+Referencia Heap's Algorithm - wikipedia
+'''
 
-def allPerms(n):
+def generate(n):
+    A = list(range(1,n+1))
+    c = [0]*n
+    yield A
+    i = 0
+    while i < n:
+        if  c[i] < i:
+            if i%2 == 0:
+                A[0],A[i] = A[i], A[0]
+            else:
+                A[c[i]],A[i] = A[i], A[c[i]]
+            yield A
+            c[i] = c[i] + 1
+            i = 0
+        else:
+            c[i] = 0
+            i = i + 1
+
+def generateM(n):
+    M = []
+    A = list(range(1,n+1))
+    c = [0]*n
+    M.append(A)
+    i = 0
+    while i < n:
+        if  c[i] < i:
+            if i%2 == 0:
+                A[0],A[i] = A[i], A[0]
+            else:
+                A[c[i]],A[i] = A[i], A[c[i]]
+            M.append(A)
+            c[i] = c[i] + 1
+            i = 0
+        else:
+            c[i] = 0
+            i = i + 1
+    return M
+
+def allPerms(n, perms):
     if(n > 8):
         print("Input too big, try a number like 8 or less")
     comparations = []
     swaps = []
     others = []
-    perms = it.permutations(range(n))
     for perm in perms:
         temp = insertionSort(list(perm))
         comparations = comparations + [temp[0]]
@@ -74,8 +121,6 @@ def allPerms(n):
         others = others + [temp[2]]
     graph(comparations, swaps, others)
     
-
-
 def samplePerms(n, m):
     comparations = []
     swaps = []
@@ -89,5 +134,7 @@ def samplePerms(n, m):
         others = others + [temp[2]]
     graph(comparations, swaps, others)
 
-samplePerms(30, 10000)
+#print(list(it.permutations(range(5))))
+#allPerms(5,it.permutations(range(5)))
+#allPerms(5,generate(5))
     
